de071059b03130538640d91f14645cd6b56a2f07
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-01-14 22:32:26 +0300

    Create User.java

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,30 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+
+@Data
+public class User {
+    private Integer id;
+
+    @NotBlank(message = "Электронная почта не может быть пустой")
+    @Email(message = "Электронная почта должна содержать символ @")
+    private String email;
+
+    @NotBlank(message = "Логин не может быть пустым")
+    @Pattern(regexp = "\\S+", message = "Логин не может содержать пробелы")
+    private String login;
+
+    private String name;
+
+    @PastOrPresent(message = "Дата рождения не может быть в будущем")
+    private LocalDate birthday;
+
+    public String getName() {
+        if (name == null || name.isBlank()) {
+            return login;
+        }
+        return name;
+    }
+}
