4bd288c3d1d16268deb6051054ec6e47a92d676d
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-02-10 21:30:07 +0300

    Update Film.java

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,22 +1,22 @@
 package ru.yandex.practicum.filmorate.model;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
-import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Data
 public class Film {
     private Integer id;
-
+    
     @NotBlank(message = "Название не может быть пустым")
     private String name;
-
+    
     @Size(max = 200, message = "Максимальная длина описания — 200 символов")
     private String description;
-
-    @NotNull(message = "Дата релиза не может быть null")
+    
+    @NotNull(message = "Дата релиза обязательна")
     private LocalDate releaseDate;
-
-    @Positive(message = "Продолжительность фильма должна быть положительным числом")
+    
+    @Positive(message = "Продолжительность фильма должна быть положительной")
     private int duration;
 }
