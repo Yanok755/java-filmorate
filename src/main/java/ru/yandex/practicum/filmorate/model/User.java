ac1254b3480cb306980c854ad50c1464f04c4416
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-02-18 13:46:40 +0300

    Update User.java

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,31 +1,14 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
-import javax.validation.constraints.*;
+
 import java.time.LocalDate;
 
 @Data
 public class User {
-    private Integer id;
-
-    @NotBlank(message = "Электронная почта не может быть пустой")
-    @Email(message = "Электронная почта должна содержать символ @")
+    private long id;
     private String email;
-
-    @NotBlank(message = "Логин не может быть пустым")
-    @Pattern(regexp = "^\\S+$", message = "Логин не может содержать пробелы")
     private String login;
-
     private String name;
-
-    @NotNull(message = "Дата рождения обязательна")
-    @PastOrPresent(message = "Дата рождения не может быть в будущем")
     private LocalDate birthday;
-
-    public String getName() {
-        if (name == null || name.isBlank()) {
-            return login;
-        }
-        return name;
-    }
 }
