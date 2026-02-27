a972bd1d669ac65552d9acaae80cbf126fa48ee1
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-02-27 23:22:10 +0300

    Update UserController.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -18,7 +18,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    private final Map<Long, User> users = new HashMap<>();
+    private static final Map<Long, User> users = new HashMap<>();
 
     @GetMapping
     public ResponseEntity<List<User>> getUsers() {
