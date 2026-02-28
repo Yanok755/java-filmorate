b79296e8db69bb923e6e80485fda326ef40661cb
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-02-28 12:16:38 +0300

    Update FilmorateApplication.java

--- src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/filmorate/FilmorateApplication.java
@@ -1,14 +1,12 @@
 package ru.yandex.practicum.filmorate;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
 @SpringBootApplication
 public class FilmorateApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(FilmorateApplication.class, args);
-        log.info("Начало работы сервера");
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(FilmorateApplication.class, args);
+	}
+
 }
