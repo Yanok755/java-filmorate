a430359cd74aa90906051e64f3c06143e3c650b2
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-02-28 12:01:02 +0300

    Update UserController.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.Exceptions.ValidationException;
+import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.ErrorResponse;
 import ru.yandex.practicum.filmorate.model.User;
 
