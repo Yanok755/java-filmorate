1068fafdcbf9368b1ad5e8823d14129102d2c957
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-02-28 11:24:16 +0300

    Update NotFoundResponse.java

--- src/main/java/ru/yandex/practicum/filmorate/NotFoundResponse.java
+++ src/main/java/ru/yandex/practicum/filmorate/NotFoundResponse.java
@@ -1,31 +1,26 @@
 package ru.yandex.practicum.filmorate;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import java.time.Instant;
+import lombok.Getter;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.ErrorResponse;
 
-@Data
 @AllArgsConstructor
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public class ErrorResponse {
-    private int status;
-    private String error;
+public class NotFoundResponse implements ErrorResponse {
+    private HttpStatusCode statusCode;
+    @Getter
     private String message;
-    private String path;
+    @Getter
     private long timestamp;
-    
-    // Конструктор для простых случаев
-    public ErrorResponse(int status, String error, String message) {
-        this.status = status;
-        this.error = error;
-        this.message = message;
-        this.timestamp = Instant.now().toEpochMilli();
+
+    @Override
+    public HttpStatusCode getStatusCode() {
+        return statusCode;
     }
-    
-    // Конструктор с путем запроса
-    public ErrorResponse(int status, String error, String message, String path) {
-        this(status, error, message);
-        this.path = path;
+
+    @Override
+    public ProblemDetail getBody() {
+        return null;
     }
 }
