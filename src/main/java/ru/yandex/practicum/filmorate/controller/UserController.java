d026a0ce7c2a686c197752b17f3ffcbd47148c73
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-03-10 18:58:10 +0300

    Update UserController.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,49 +1,33 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
 
-import java.time.LocalDate;
 import java.util.Collection;
 
 @Slf4j
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public User createUser(@RequestBody User user) {
-        validate(user, "создании");
-
-        User createUser = userService.createUser(user);
-        log.info("Пользователь {} успешно создан", user.getId());
-
-        return createUser;
+    public User createUser(@Valid @RequestBody User user) {
+        return userService.createUser(user);
     }
 
     @PutMapping
     @ResponseStatus(HttpStatus.OK)
-    public User updateUser(@RequestBody User newUser) {
-        validate(newUser, "обновлении");
-
-        User createdUser = userService.updateUser(newUser);
-
-        log.info("Пользователь {} успешно обновлен", createdUser.getId());
-
-        return createdUser;
+    public User updateUser(@Valid @RequestBody User user) {
+        return userService.updateUser(user);
     }
 
     @GetMapping
@@ -80,37 +64,5 @@ public class UserController {
     @ResponseStatus(HttpStatus.OK)
     public Collection<User> getCommonFriends(@PathVariable Long id, @PathVariable Long otherId) {
         return userService.getCommonFriends(id, otherId);
-    }
-
-    private void validate(User user, String info) {
-        if (user.getEmail() == null || user.getEmail().isBlank()) {
-            log.error("Ошибка валидации при {}: электронная почта не должна быть пустой", info);
-            throw new ValidationException("Электронная почта не должна быть пустой");
-        }
-
-        if (!user.getEmail().contains("@")) {
-            log.error("Ошибка валидации при {}: email {} не содержит символ @", info, user.getEmail());
-            throw new ValidationException("Электронная почта должна содержать символ @");
-        }
-
-        if (user.getLogin() == null || user.getLogin().isBlank()) {
-            log.error("Ошибка валидации при {}: логин не должен быть пустым", info);
-            throw new ValidationException("Логин не может быть пустым");
-        }
-
-        if (user.getLogin().contains(" ")) {
-            log.error("Ошибка валидации при {}: логин {} содержит пробелы", info, user.getLogin());
-            throw new ValidationException("Логин не может содержать пробелы");
-        }
-
-        if (user.getName() == null || user.getName().isBlank()) {
-            log.info("Имя пользователя не указано при {}, используем логин: {}", info, user.getLogin());
-            user.setName(user.getLogin());
-        }
-
-        if (user.getBirthday().isAfter(LocalDate.now())) {
-            log.error("Ошибка валидации при {}: дата рождения {} в будущем", info, user.getBirthday());
-            throw new ValidationException("Дата рождения не может быть в будущем");
-        }
     }
 }
