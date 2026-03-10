8b0b31a8636b19eb50647d3b13491003c6ee2b0d
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-03-10 18:55:34 +0300

    Update FilmController.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,59 +1,34 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 
-import java.time.LocalDate;
 import java.util.Collection;
 
 @Slf4j
 @RestController
-@Validated
 @RequestMapping("/films")
+@RequiredArgsConstructor
 public class FilmController {
 
     private final FilmService filmService;
-    LocalDate minReleaseDate = LocalDate.of(1895, 12, 28);
-
-    @Autowired
-    public FilmController(FilmService filmService) {
-        this.filmService = filmService;
-    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Film createFilm(@RequestBody Film film) {
-
-        validate(film, "создании");
-
-        Film createdFilm = filmService.createFilm(film);
-        log.info("Фильм {} успешно создан", createdFilm.getId());
-
-        return createdFilm;
+    public Film createFilm(@Valid @RequestBody Film film) {
+        return filmService.createFilm(film);
     }
 
     @PutMapping
     @ResponseStatus(HttpStatus.OK)
-    public Film updateFilm(@RequestBody Film newFilm) {
-        if (newFilm.getId() == null) {
-            log.error("ID фильма не может быть null при обновлении");
-            throw new ValidationException("ID фильма должен быть указан");
-        }
-
-        validate(newFilm, "обновлении");
-
-       Film updateFilm = filmService.updateFilm(newFilm);
-
-        log.info("Фильм с id {} успешно обновлен", newFilm.getId());
-
-        return updateFilm;
+    public Film updateFilm(@Valid @RequestBody Film film) {
+        return filmService.updateFilm(film);
     }
 
     @GetMapping
@@ -83,41 +58,7 @@ public class FilmController {
     @GetMapping("/popular")
     @ResponseStatus(HttpStatus.OK)
     public Collection<Film> getMostPopularFilms(@RequestParam(defaultValue = "10")
-                                                    @Positive(message = "Количество фильмов должно быть положительным") Integer count) {
-        if (count == null || count <= 0) {
-            log.error("Некорректное количество: {}", count);
-            throw new ValidationException("Количество фильмов должно быть положительным");
-        }
+                                                @Positive(message = "Количество фильмов должно быть положительным") Integer count) {
         return filmService.getMostPopularFilms(count);
-    }
-
-    private void validate(Film film, String info) {
-        if (film.getName() == null || film.getName().isBlank()) {
-            log.error("Ошибка валидации при {}: название фильма не должно быть пустым", info);
-            throw new ValidationException("Имя фильма не должно быть пустым");
-        }
-
-        if (film.getDescription() != null && film.getDescription().length() > 200) {
-            log.error("Ошибка валидации при {}: длина описания {} превышает 200 символов", info,
-                    film.getDescription().length());
-
-            throw new ValidationException("Максимальная длина описания — 200 символов");
-        }
-
-        if (film.getReleaseDate() == null) {
-            log.error("Ошибка валидации при {}: дата релиза не указана", info);
-            throw new ValidationException("Дата релиза должна быть указана");
-        }
-
-        if (film.getReleaseDate().isBefore(minReleaseDate)) {
-            log.error("Ошибка валидации при {}: дата релиза {} раньше минимальной {}", info,
-                    film.getReleaseDate(), minReleaseDate);
-            throw new ValidationException("Дата релиза не может быть раньше 28 декабря 1895 года");
-        }
-
-        if (film.getDuration() != null && film.getDuration() <= 0) {
-            log.error("Ошибка валидации при {}: продолжительность фильма {} должна быть положительной", info, film.getDuration());
-            throw new ValidationException("Продолжительность фильма должна быть положительным числом");
-        }
     }
 }
