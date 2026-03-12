1d12b9cad4e8276fe669c1f0fa770473c2cad021
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-03-13 00:19:56 +0300

    Update GenreRowMapper.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/mapper/GenreRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/mapper/GenreRowMapper.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.filmorate.storage.mapper;
+package ru.yandex.practicum.filmorate.storage.genre.mapper;
 
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
@@ -13,8 +13,8 @@ public class GenreRowMapper implements RowMapper<Genre> {
     @Override
     public Genre mapRow(ResultSet rs, int rowNum) throws SQLException {
         Genre genre = new Genre();
-        genre.setId(rs.getInt("genre_id"));
-        genre.setName(rs.getString("genre_name"));
+        genre.setId(rs.getInt("id"));
+        genre.setName(rs.getString("name"));
         return genre;
     }
 }
