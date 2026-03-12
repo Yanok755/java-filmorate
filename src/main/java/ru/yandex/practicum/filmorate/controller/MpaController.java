d2591b69060a68027ebf019c1cf996f57163f633
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-03-12 23:52:03 +0300

    Update MpaController.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
@@ -3,9 +3,8 @@ package ru.yandex.practicum.filmorate.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Mpa;
-import ru.yandex.practicum.filmorate.storage.MpaStorage;
+import ru.yandex.practicum.filmorate.service.MpaService;
 
 import java.util.List;
 
@@ -15,18 +14,17 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MpaController {
 
-    private final MpaStorage mpaStorage;
+    private final MpaService mpaService;
 
     @GetMapping
     public List<Mpa> getAllMpa() {
         log.info("Запрос на получение всех рейтингов MPA");
-        return mpaStorage.findAll();
+        return mpaService.getAllMpa();
     }
 
     @GetMapping("/{id}")
-    public Mpa getMpaById(@PathVariable int id) {
+    public Mpa getMpaById(@PathVariable Integer id) {
         log.info("Запрос на получение рейтинга MPA с id {}", id);
-        return mpaStorage.findById(id)
-                .orElseThrow(() -> new NotFoundException("Рейтинг mpa с id " + id + " не найден"));
+        return mpaService.getMpaById(id);
     }
 }
