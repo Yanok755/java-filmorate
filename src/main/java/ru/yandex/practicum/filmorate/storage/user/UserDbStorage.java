a3046edd5d55ba6c6451b29f2b357c04b43b5dc3
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-03-13 00:32:55 +0300

    Update UserDbStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
@@ -171,7 +171,7 @@ public class UserDbStorage implements UserStorage {
         String sql = """
             SELECT u.* FROM users u
             WHERE u.id IN (
-                SELECT f1.friend_id 
+                SELECT f1.friend_id
                 FROM friendships f1
                 INNER JOIN friendships f2 ON f1.friend_id = f2.friend_id
                 WHERE f1.user_id = ? AND f2.user_id = ?
