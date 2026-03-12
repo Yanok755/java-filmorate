0090b17ce2316ba2186016197b88b1ef078b537b
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-03-12 19:39:23 +0300

    Update FilmStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -1,21 +1,22 @@
 package ru.yandex.practicum.filmorate.storage.film;
 
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.Mpa;
+
 import java.util.Collection;
 import java.util.Optional;
+import java.util.Set;
 
 public interface FilmStorage {
     Film createFilm(Film film);
-
     Film updateFilm(Film film);
-
     Collection<Film> findAllFilms();
-
     Optional<Film> getFilmById(Long id);
-
-    boolean deleteFilm(Long id);
-
     boolean containsFilm(Long id);
-
-    int getFilmsCount();
+    void addLike(Long filmId, Long userId);
+    void removeLike(Long filmId, Long userId);
+    Collection<Film> getMostPopularFilms(Integer limit);
+    int getLikesCount(Long filmId);
+    Set<Long> getFilmLikes(Long filmId);
+    Mpa getMpaById(Integer id);
 }
