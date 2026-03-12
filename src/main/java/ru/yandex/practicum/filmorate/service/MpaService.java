ed698f87e5059df1e50eefca0933c75b17f68bb2
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-03-12 19:13:44 +0300

    Create MpaService.java

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/MpaService.java
@@ -0,0 +1,33 @@
+package ru.yandex.practicum.filmorate.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.Mpa;
+import ru.yandex.practicum.filmorate.storage.MpaStorage;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MpaService {
+
+    private final MpaStorage mpaStorage;
+
+    public List<Mpa> getAllMpa() {
+        log.debug("Получение всех рейтингов MPA");
+        return mpaStorage.findAll();
+    }
+
+    public Mpa getMpaById(int id) {
+        log.debug("Получение рейтинга MPA с id: {}", id);
+
+        return mpaStorage.findById(id)
+                .orElseThrow(() -> {
+                    log.error("Рейтинг MPA с id {} не найден", id);
+                    return new NotFoundException("Рейтинг MPA с id " + id + " не найден");
+                });
+    }
+}
