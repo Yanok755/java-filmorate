c1918462a58d632e68e0bfc68e665403e11233b4
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-03-12 19:09:21 +0300

    Update GenreController.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
@@ -3,9 +3,8 @@ package ru.yandex.practicum.filmorate.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Genre;
-import ru.yandex.practicum.filmorate.storage.GenreStorage;
+import ru.yandex.practicum.filmorate.service.GenreService;
 
 import java.util.List;
 
@@ -15,18 +14,17 @@ import java.util.List;
 @RequiredArgsConstructor
 public class GenreController {
 
-    private final GenreStorage genreStorage;
+    private final GenreService genreService;
 
     @GetMapping
     public List<Genre> getAllGenres() {
         log.info("Запрос на получение всех жанров");
-        return genreStorage.findAll();
+        return genreService.getAllGenres();
     }
 
     @GetMapping("/{id}")
     public Genre getGenreById(@PathVariable int id) {
         log.info("Запрос на получение жанра с id {}", id);
-        return genreStorage.findById(id)
-                .orElseThrow(() -> new NotFoundException("Жанр с id " + id + " не найден"));
+        return genreService.getGenreById(id);
     }
 }
