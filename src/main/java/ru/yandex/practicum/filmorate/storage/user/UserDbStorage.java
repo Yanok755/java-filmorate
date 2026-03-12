04f3ede174e11d163fdee5ec8c7969e74a38f0bc
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-03-13 00:56:06 +0300

    Update UserDbStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserDbStorage.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.FriendshipStatus;
 import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.storage.user.mapper.UserRowMapper;
+import ru.yandex.practicum.filmorate.storage.mapper.UserRowMapper;
 
 import java.sql.PreparedStatement;
 import java.sql.Statement;
