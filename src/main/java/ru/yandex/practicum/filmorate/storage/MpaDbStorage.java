09d01714c5c07a2e2e1d3c24187356c56c7b1978
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-03-13 00:57:36 +0300

    Update MpaDbStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/MpaDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/MpaDbStorage.java
@@ -4,7 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.model.Mpa;
-import ru.yandex.practicum.filmorate.storage.mpa.mapper.MpaRowMapper;
+import ru.yandex.practicum.filmorate.storage.mapper.MpaRowMapper;
 
 import java.util.List;
 import java.util.Optional;
