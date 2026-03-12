5e30482457a42697201881b6bb2f91f36374497b
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-03-12 19:51:51 +0300

    Update FilmStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -9,14 +9,24 @@ import java.util.Set;
 
 public interface FilmStorage {
     Film createFilm(Film film);
+
     Film updateFilm(Film film);
+
     Collection<Film> findAllFilms();
+
     Optional<Film> getFilmById(Long id);
+
     boolean containsFilm(Long id);
+
     void addLike(Long filmId, Long userId);
+
     void removeLike(Long filmId, Long userId);
+
     Collection<Film> getMostPopularFilms(Integer limit);
+
     int getLikesCount(Long filmId);
+
     Set<Long> getFilmLikes(Long filmId);
+
     Mpa getMpaById(Integer id);
 }
