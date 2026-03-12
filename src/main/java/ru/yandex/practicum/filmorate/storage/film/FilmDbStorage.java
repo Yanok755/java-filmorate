1f2829526944aad38fb49d0e8df96b8f290cc498
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-03-13 00:31:11 +0300

    Update FilmDbStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmDbStorage.java
@@ -10,7 +10,6 @@ import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Genre;
-import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.storage.film.mapper.FilmRowMapper;
 import ru.yandex.practicum.filmorate.storage.MpaStorage;
 
