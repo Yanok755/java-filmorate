7d0a4d19e5ba8e134cab6da78f87bf6b3ac786fb
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-03-12 15:04:39 +0300

    Update Film.java

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,9 +1,7 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
-
 import java.time.LocalDate;
-import java.util.HashSet;
 import java.util.Set;
 
 @Data
@@ -13,7 +11,6 @@ public class Film {
     private String description;
     private LocalDate releaseDate;
     private Integer duration;
-    private Set<Long> likes = new HashSet<>();
-    private Set<Genre> genres = new HashSet<>();
-    private MpaRating mpaRating;
+    private Set<Genre> genres;
+    private Mpa mpa;
 }
