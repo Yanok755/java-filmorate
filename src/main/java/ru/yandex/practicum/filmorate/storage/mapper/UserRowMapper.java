6a815c1e971ff70561d1cdc2798e6928ad196fbd
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-03-13 10:12:53 +0300

    Update UserRowMapper.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/mapper/UserRowMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/mapper/UserRowMapper.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.filmorate.storage.user.mapper;
+package ru.yandex.practicum.filmorate.storage.mapper;
 
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
