09544b5bb6d2c7f910acc77117532438f33f11aa
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-03-13 10:01:44 +0300

    Update FilmControllerTest.java

--- src/test/java/ru/yandex/practicum/filmorate/filmTests/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/filmTests/FilmControllerTest.java
@@ -34,16 +34,17 @@ class FilmControllerTest {
     void setUp() {
         filmStorage = new InMemoryFilmStorage();
         userStorage = new InMemoryUserStorage();
-        filmService = new FilmService(filmStorage, userStorage);
-
         genreStorage = mock(GenreStorage.class);
         mpaStorage = mock(MpaStorage.class);
+        filmService = new FilmService(filmStorage, userStorage, genreStorage, mpaStorage);
+
+
 
         when(mpaStorage.existsById(3)).thenReturn(true);
         when(genreStorage.existsById(1)).thenReturn(true);
         when(genreStorage.existsById(6)).thenReturn(true);
 
-        filmController = new FilmController(filmService, genreStorage, mpaStorage);
+        filmController = new FilmController(filmService);
 
         Mpa mpa = new Mpa();
         mpa.setId(3);
