9e31f60d9be2a446cb782d7f0ba8348b12b6fee4
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-03-14 09:30:48 +0300

    Update UserStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -2,9 +2,12 @@ package ru.yandex.practicum.filmorate.storage.user;
 
 import ru.yandex.practicum.filmorate.model.User;
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public interface UserStorage {
+
     User createUser(User user);
 
     User updateUser(User user);
@@ -18,4 +21,12 @@ public interface UserStorage {
     boolean containsUser(Long id);
 
     int getUsersCount();
+
+    void addFriend(Long userId, Long friendId);
+
+    void removeFriend(Long userId, Long friendId);
+
+    Collection<User> getUserFriends(Long userId);
+
+    Collection<User> getCommonFriends(Long userId, Long otherUserId);
 }
