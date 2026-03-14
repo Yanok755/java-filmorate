c928e1fee19238128f2f03b47ce55a094369d901
Yanok755 <161292231+Yanok755@users.noreply.github.com>
2026-03-14 09:02:59 +0300

    Update UserStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -1,10 +1,7 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
-import ru.yandex.practicum.filmorate.model.FriendshipStatus;
 import ru.yandex.practicum.filmorate.model.User;
-
 import java.util.Collection;
-import java.util.Map;
 import java.util.Optional;
 
 public interface UserStorage {
@@ -21,24 +18,4 @@ public interface UserStorage {
     boolean containsUser(Long id);
 
     int getUsersCount();
-
-    // Методы для работы с друзьями
-    void addFriend(Long userId, Long friendId);
-
-    void removeFriend(Long userId, Long friendId);
-
-    Collection<Long> getFriendIds(Long userId);
-
-    Map<Long, FriendshipStatus> getFriendsWithStatus(Long userId);
-
-    Collection<User> getFriendsAsUsers(Long userId);
-
-    Collection<User> getCommonFriends(Long userId, Long otherId);
-
-    boolean areFriends(Long userId, Long otherId);
-
-    int getFriendsCount(Long userId);
-
-    void updateFriendshipStatus(Long userId, Long friendId, FriendshipStatus status);
 }
-
